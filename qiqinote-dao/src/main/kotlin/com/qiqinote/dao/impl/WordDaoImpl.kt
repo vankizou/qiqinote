1a907445cc7ac6350446188ee0f5bbc56270748b
zoufanqi <zoufanqi@zhugeio.com>
2018-10-08 19:02:28 +0800

    v

--- qiqinote-dao/src/main/kotlin/com/qiqinote/dao/impl/WordDaoImpl.kt
+++ qiqinote-dao/src/main/kotlin/com/qiqinote/dao/impl/WordDaoImpl.kt
@@ -22,7 +22,7 @@ class WordDaoImpl @Autowired constructor(
                 StringUtil.isBlank(word.from) ||
                 StringUtil.isBlank(word.word) ||
                 word.word!!.length > 500) return 0
-        if (this.exists(word)) return 0
+        if (this.exists(word.word!!)) return 0
 
         val paramMap = mapOf("word" to word.word!!.trim(), "from" to word.from!!.trim(), "type" to word.type?.trim())
         val sql = "INSERT INTO word(`type`, `from`, `word`) VALUES(:type, :from, :word)"
@@ -37,9 +37,9 @@ class WordDaoImpl @Autowired constructor(
         return if (list.isEmpty()) "每天进步一点点！" else list[0].word!!
     }
 
-    private fun exists(word: Word): Boolean {
-        val paramMap = mapOf("type" to word.type, "from" to word.from, "word" to word.word)
-        val sql = "SELECT COUNT(1) FROM word WHERE type=:type AND `from`=:from AND word=:word"
+    private fun exists(word: String): Boolean {
+        val paramMap = mapOf("word" to word)
+        val sql = "SELECT COUNT(1) FROM word WHERE word=:word"
         val count = this.namedParameterJdbcTemplate.queryForObject(sql, paramMap, Long::class.java) ?: 0
         return count > 0
     }
