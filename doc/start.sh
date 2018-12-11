0bfc45d4bce1845a944355f4294537c3d7fafcc9
zoufanqi <zoufanqi@zhugeio.com>
2018-12-11 15:01:12 +0800

    v

--- doc/start.sh
+++ doc/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup java -jar -Dspring.config.location=/data/vanki/www/config.properties web-1.0-SNAPSHOT.war > /dev/null 2>&1 &
+nohup java -Xmx1g -XX:+UseG1GC -jar -Dspring.config.location=/data/vanki/www/config.properties web-1.0-SNAPSHOT.war > /dev/null 2>&1 &
